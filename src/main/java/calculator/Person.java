2730fa83caa37a110c8bd6283f8b122768115d83
LeeJiWon <dlwldnjs1009@gmail.com>
2025-02-18 16:53:58 +0900

    feat: 주사위 굴리는 도중 발생한 예외처리 구현

--- src/main/java/calculator/Person.java
+++ src/main/java/calculator/Person.java
@@ -4,13 +4,17 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
 public class Person {
-    
+
     public Future<Integer> rollTheDice() {
         CompletableFuture<Integer> futureDice = new CompletableFuture<>();
         new Thread(() -> {
-            Dice dice = Dice.createDice(new RandomNumber()); // 다른 스레드에서 비동기적으로 계산을 수행한다.
-            futureDice.complete(dice.roll());
+            try {
+                Dice dice = Dice.createDice(new RandomNumber()); // 다른 스레드에서 비동기적으로 계산을 수행한다.
+                futureDice.complete(dice.roll());
+            } catch (Exception ex) {
+                futureDice.completeExceptionally(ex); // 도중에 문제가 발생하면 발생한 에러를 포함시켜 Future를 종료.
+            }
         }).start();
-        return futureDice; // 계산 결과가 완료되길 기다리지 않고 Future를 반환.
+        return futureDice;
     }
 }
