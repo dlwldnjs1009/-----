9f76f4a4d79e926e0a7f0bc7008d1b0ca32922b8
LeeJiWon <dlwldnjs1009@gmail.com>
2025-02-18 16:06:47 +0900

    feat: 스레드풀 이용한 주사위 던지기 구현
    이 방식은 여러 사람이 공유되는 주사위 개수를 가지고 주사위를 한 번씩 던진다고 생각함.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,6 +7,18 @@ import java.util.stream.IntStream;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("주사위 결과: " + Person.rollTheDice());
+
+        ThreadPoolExecutor executorService = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+        IntStream.range(0, 12).forEach(n -> executorService.execute(() ->
+        {
+            String threadName = Thread.currentThread().getName();
+            int pips = Person.rollTheDice(); // pips는 주사위눈을 뜻한다.
+            System.out.println("던지는 사람(스레드):" + threadName + ", 주사위 눈: " + pips);
+
+//            System.out.println("스레드 풀 사이즈: " + executorService.getPoolSize());
+//            System.out.println("Queue 사이즈: " + executorService.getQueue().size());
+
+        }));
+        executorService.shutdown();
     }
 }
