b7ca1bb293d262decc25d97d7a39374ddab8a4f0
LeeJiWon <dlwldnjs1009@gmail.com>
2025-02-18 17:15:59 +0900

    feat: 매번 다른 pool 스레드 활용해 주사위 굴리기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -11,7 +11,11 @@ public class Application {
         long invocationTime = ((System.nanoTime() - start) / 1_000_000);
         System.out.println("Invocation returned after " + invocationTime + " msecs");
 
-        dosometing(person); // 주사위 굴리는 동안 다른 작업을 수행
+        dosometing(person); // 주사위 굴리는 동안 또 다른 주사위 굴리기 작업을 수행
+        dosometing(person);
+        dosometing(person);
+        dosometing(person);
+        dosometing(person);
 
         try {
             int result = futurePips.get();
@@ -25,13 +29,11 @@ public class Application {
 
     private static void dosometing(Person person) {
         System.out.println("dosometing() 시작: 다른 작업 수행 중, 동시에 두 번째 주사위 굴리기...");
-        // 두 번째 비동기 주사위 굴리기
+        //비동기 주사위 굴리기
         Future<Integer> futureDice2 = person.rollTheDice();
 
         // dosometing()에서 다른 작업을 수행할 수 있음
-        // (예: 간단한 지연 또는 로깅 작업 등)
         try {
-            // 두 번째 주사위 결과를 받아서 출력
             int dice2 = futureDice2.get();
             System.out.println("dosometing()에서 두 번째 주사위 눈: " + dice2);
         } catch (Exception e) {
--- src/main/java/calculator/Person.java
+++ src/main/java/calculator/Person.java
@@ -1,13 +1,18 @@
 package calculator;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Person {
 
     public Future<Integer> rollTheDice() {
         Dice dice = Dice.createDice(new RandomNumber());
-        return CompletableFuture.supplyAsync(dice::roll);
+        return CompletableFuture.supplyAsync(() -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println("rollTheDice 실행 중인 스레드: " + threadName);
+            return dice.roll();
+        });
     }
 
 
